696c83b3660ba628cde7667f93a276d9480db5b1
Haitham Naksho <hnaksho@oakland.edu>
2025-03-22 21:06:47 -0400

    free wili processor type

--- sendfile.py
+++ sendfile.py
@@ -6,3 +6,12 @@ from freewili.types import FreeWiliProcessorType
 first_device = freewili.find_all(FreeWiliProcessorType.Main)[0]
 # Send the file to the Main Processor.
 first_device.send_file("my_script.wasm", "/scripts/my_script.wasm").unwrap()
+
+# #    from freewili.serial_util import find_all
+#     from freewili.types import FreeWiliProcessorType
+#
+#     # Find the first Main Processor.
+#     first_device = freewili.find_all(FreeWiliProcessorType.Main)[0]
+#     # Send the file to the Main Processor.
+#     input(f"WARNING: This will force the processor for {first_device} into UF2 bootloader, press any key to continue...")
+#     first_device.reset_to_uf2_bootloader().unwrap()
