699aa6da854302e050f75086a58daf39a63cd5c6
Haitham Naksho <hnaksho@oakland.edu>
2025-03-22 21:10:05 -0400

    added

--- sendfile.py
+++ sendfile.py
@@ -3,9 +3,9 @@ from freewili.serial_util import find_all
 from freewili.types import FreeWiliProcessorType
 
 # Find the first Main Processor.
-first_device = freewili.find_all(FreeWiliProcessorType.Main)[0]
+first_device = freewili.find_all(FreeWiliProcessorType.Main)[1]
 # Send the file to the Main Processor.
-first_device.send_file("my_script.wasm", "/scripts/my_script.wasm").unwrap()
+first_device.send_file("willy.wav", "/sounds/willy.wav").unwrap()
 
 # #    from freewili.serial_util import find_all
 #     from freewili.types import FreeWiliProcessorType
